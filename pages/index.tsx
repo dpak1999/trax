151ca08b5eed6870cb41560fc94345b061e1178d
deepak <deepakkumardash313@gmail.com>
2022-04-29 23:49:11 +0530

    setting up eslint & typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+/** @format */
 
-export default function Home() {
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -65,5 +67,7 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;

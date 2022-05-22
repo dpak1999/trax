a265cfb865a2de68a8501f7c46a9fad3a9973eec
deepak <deepakkumardash313@gmail.com>
2022-05-23 00:21:43 +0530

    add : logo and sidebar layout

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,13 +1,13 @@
 /** @format */
 
 import { Box } from '@chakra-ui/layout';
-import React from 'react';
+import Sidebar from './Sidebar';
 
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        sidebar
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         {children}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import NextImage from 'next/image';
+import { Box } from '@chakra-ui/layout';
+import {} from 'react-icons/md';
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="grey"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/logo.svg" height={60} width={120} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;

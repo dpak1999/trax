b132713b8fee79543a1e9a656ce65510ace4cfc5
deepak <deepakkumardash313@gmail.com>
2022-05-23 21:40:40 +0530

    update schema & add seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+/** @format */
+
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import { artistsData } from './songsData';
+
+const prisma = new PrismaClient();
+
+const runScript = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+};
+
+runScript()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });

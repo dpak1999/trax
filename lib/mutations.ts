7d9485288fce2c2c4e1a1dce715212a76fc22133
deepak <deepakkumardash313@gmail.com>
2022-05-28 23:22:09 +0530

    add : mutation & fetcher functions

---
+++ lib/mutations.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+import fetcher from './fetcher';
+
+export const auth = (
+  mode: 'signin' | 'signup',
+  body: { email: string; password: string }
+) => {
+  return fetcher(`/${mode}`, body);
+};

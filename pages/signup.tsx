10a1986940f792ea84ce67b008c960e43b1e1256
deepak <deepakkumardash313@gmail.com>
2022-05-28 23:57:29 +0530

    add : auth form & signin signup page

---
+++ pages/signup.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from 'react';
+import AuthForm from '../components/AuthForm';
+
+const SignUp = () => {
+  return <AuthForm mode="signup" />;
+};
+
+SignUp.authPage = true;
+
+export default SignUp;
